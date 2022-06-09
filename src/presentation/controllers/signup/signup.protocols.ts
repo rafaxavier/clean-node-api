43f3df1cf6cbb24f87548fd435bdd3021a288a76
rafael <rafaworksi@outlook.com>
2022-06-08 23:02:07 -0300

    refactor: move all signup protocols to a sigle file

---
+++ src/presentation/controllers/signup/signup.protocols.ts
@@ -0,0 +1,5 @@
+// protocolos genericos
+export * from '../../protocols'
+export * from '../../protocols/email-validator'
+export * from '../../../domain/usecases/add-account'
+export * from '../../../domain/models/account'
